d30ff8306813f86bc757c401bd9f239717129b41
serverkasap <server.kasap@outlook.com>
2022-08-23 11:33:21 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="paddle",  ## This will be the name your package will be published with
+    version="1.0",
+    packages=find_packages(),  # This one is important to explain. See the notebook for a detailed explanation
+    install_requires=[
+        "selenium",
+        "webdriver_manager",
+    ],  # For this project we are using two external libraries
+    # Make sure to include all external libraries in this argument
+)
---
+++ utils/paddle.py
@@ -0,0 +1,16 @@
+import time
+from typing import Optional
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver.get("https://www.worldpadeltour.com/")
